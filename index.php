78337eb52f50a4383cbef2ae8a3f2c6785a33a66
habibgili004 <habibgili004@gmail.com>
2023-10-22 21:00:45 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo soluta quasi delectus possimus ad excepturi hic a magni facere voluptatem! Quibusdam excepturi ducimus iste, laudantium praesentium labore atque deleniti dolorum!</p>
+    <ul>
+        <li>Habib Gili ajiwinata</li>
+    </ul>
 </body>
 </html>
